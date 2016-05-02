d6003d7ffdb21d276069ee3c9b895c05c0dc18ec
Nathan Black <nathan@nathanblack.org>
2016-05-02 09:30:48 -0700

    Update Function#bind polyfil to be up to spec

--- browser-shim.js
+++ browser-shim.js
@@ -1,16 +1,21 @@
 (function(){
 
-  // A shim for non ES5 supporting browsers, like PhantomJS. Lovingly inspired by:
-  // http://www.angrycoding.com/2011/09/to-bind-or-not-to-bind-that-is-in.html
-  if (!('bind' in Function.prototype)) {
-    Function.prototype.bind = function() {
-      var funcObj = this;
-      var extraArgs = Array.prototype.slice.call(arguments);
-      var thisObj = extraArgs.shift();
-      return function() {
-        return funcObj.apply(thisObj, extraArgs.concat(Array.prototype.slice.call(arguments)));
-      };
-    };
+  if (typeof Function.prototype.bind !== 'function') {
+    Function.prototype.bind = function bind(obj) {
+      var args = Array.prototype.slice.call(arguments, 1),
+        self = this,
+        nop = function() {},
+        bound = function() {
+          return self.apply(
+            this instanceof nop ? this : (obj || {}), args.concat(
+              Array.prototype.slice.call(arguments)
+            )
+          )
+        }
+      nop.prototype = this.prototype || {}
+      bound.prototype = new nop()
+      return bound
+    }
   }
 
   function isFileReady(readyState) {
